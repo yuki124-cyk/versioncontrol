345af5f2e733590fd5ebae0ea64cd18eb5e0c8fd
Cazyn-Lim <cazynlim@gmail.com>
2026-06-23 09:33:35 +0800

    Create Chiazee.dart

---
+++ lib/Chiazee.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+// This is a comment from Chiazee
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World! This is created by Chiazee'),
+        ),
+      ),
+    );
+  }
+}
